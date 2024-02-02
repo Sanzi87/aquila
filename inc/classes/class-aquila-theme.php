1de0c580e0250b454fca924b0da338180a716bf9
Alexander Blomberg <info@dalasoftware.se>
2024-02-02 21:21:34 +0100

    Add title-tag and custom-logo

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -26,5 +26,19 @@ class AQUILA_THEME
         /**
          * Actions.
          */
+        add_action('after_setup_theme', [$this, 'setup_theme']);
+    }
+
+    public function setup_theme()
+    {
+        add_theme_support('title-tag');
+        add_theme_support('custom-logo', [
+            'header-text'          => ['site-title', 'site-description'],
+            'height'               => 100,
+            'width'                => 400,
+            'flex-height'          => true,
+            'flex-width'           => true,
+            'unlink-homepage-logo' => true,
+        ]);
     }
 }
